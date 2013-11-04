1836f32d044d5ad80d9b47bed1e696ed22805ee8
jsantangelo <jamesfsantangelo@gmail.com>
2013-11-04 13:39:00 -0500

    Added back database application stuff.

--- SecurityHelper/src/com/missionse/securityhelper/SecurityHelperApplication.java
+++ SecurityHelper/src/com/missionse/securityhelper/SecurityHelperApplication.java
@@ -5,33 +5,25 @@ import android.app.Application;
 import com.missionse.database.GenericDatabase;
 import com.missionse.securityhelper.database.model.BuildingLocation;
 import com.missionse.securityhelper.database.model.CubeInfo;
-import com.missionse.securityhelper.database.model.ExitDoor;
 import com.missionse.securityhelper.database.model.History;
 import com.missionse.securityhelper.database.model.Person;
 
 public class SecurityHelperApplication extends Application {
-	
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		//GenericDatabase.initialize(
-		//		getApplicationContext(), /* Context */ 
-		//		"default.db",            /* Database name */
-		//		1,                       /* Database version */
-		//		new Class<?>[]{          /* Entity Classes  */ 
-		//					BuildingLocation.class, 
-		//	                CubeInfo.class,
-		//	                ExitDoor.class,
-		//	                History.class,
-		//	                Person.class,
-		//	                });
+		GenericDatabase.initialize(getApplicationContext(), /* Context */
+				"default.db", /* Database name */
+				1, /* Database version */
+				new Class<?>[] { /* Entity Classes */
+				BuildingLocation.class, CubeInfo.class, History.class, Person.class, });
+
 	}
 
 	@Override
 	public void onTerminate() {
 		super.onTerminate();
 	}
-	
-		
 
 }
