a6aacda48b27841f1219c8db8cfca99d82886041
jsantangelo <jamesfsantangelo@gmail.com>
2013-11-04 09:40:59 -0500

    Added initial database activity.

--- SecurityHelper/src/com/missionse/securityhelper/SecurityHelper.java
+++ SecurityHelper/src/com/missionse/securityhelper/SecurityHelper.java
@@ -1,22 +1,40 @@
 package com.missionse.securityhelper;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.util.List;
 
-public class SecurityHelper extends Activity {
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SecurityHelper extends PreferenceActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_security_helper);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public void onBuildHeaders(final List<Header> target) {
+		loadHeadersFromResource(R.xml.security_helper_main_menu, target);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(final Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.security_helper, menu);
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(final MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_settings:
+				// TODO: handle settings
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
 }
---
+++ SecurityHelper/src/com/missionse/securityhelper/database/DatabaseActivity.java
@@ -0,0 +1,7 @@
+package com.missionse.securityhelper.database;
+
+import android.app.Activity;
+
+public class DatabaseActivity extends Activity {
+
+}
